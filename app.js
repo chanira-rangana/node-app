d0e3ba9a377a43ac9262dc3d2a20387b4fa693e1
Chanira <chanirapannala@gmail.com>
2024-05-26 13:28:09 +0530

    get

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "ns..3" });
+  res.status(200).json({ message: "get /" });
 });
 
 app.listen(PORT, () =>
